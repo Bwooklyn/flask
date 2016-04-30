ce856c3c1d80d444355f94c9533251d3de15049c
william <bwooklynracer@gmail.com>
2016-04-30 11:58:53 -0400

    blagh

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template, request, session, redirect, url_for, e
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home", method =["get", "POST"])
 
 def home():
-    return "<h1> HELLO WORLD </h1>"
+    if request.method=="POST":
+        form = requestfrom
+        number = form["number"]
+        chamber = form["chamber"]
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = "V\xd7\x94<\xb50\xca\n\xf9\xa0@\x17\x06(\x17-\x8f\xf39\x83\xa2\xfcm\x14"
-    app.run('0.0.0.0', port=8000)
+    app.run('0.0.0.0', port=7000)
 
